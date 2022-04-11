1724fc2a3f4f80491d869c565bc5820780d47ea8
Matheus <matheuspsilva29@gmail.com>
2022-04-11 18:49:21 -0300

    Criando entidades, tabelas e DAO genérico

--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,11 +2,24 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 	
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setIdade(45);
+		pessoa.setLogin("teste");
+		pessoa.setNome("teste2");
+		pessoa.setSobrenome("abc");
+		pessoa.setEmail("teste@teste.com");
+		pessoa.setSenha("1234");
+		
+		daoGeneric.salvar(pessoa);
+		
 	}
 
 }
